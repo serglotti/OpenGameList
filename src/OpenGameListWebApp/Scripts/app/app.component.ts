769539884656da8ced5e52d0ac03756577c4d2f1
serglotti <sergio.lottimore@gmail.com>
2016-10-31 20:22:42 -0300

    SKELETON - Final

--- src/OpenGameListWebApp/Scripts/app/app.component.ts
+++ src/OpenGameListWebApp/Scripts/app/app.component.ts
@@ -1,6 +1,6 @@
 ﻿import {Component} from "@angular/core";
 @Component({
  selector: "opengamelist",
- template: `<h1>OpenGameList</h1><div>Work in progress...</div>`
+ template: `<h1>OpenGameList</h1><div>Who loves Vicky? Serg does. Mucho...</div>`
 })
 export class AppComponent { }
