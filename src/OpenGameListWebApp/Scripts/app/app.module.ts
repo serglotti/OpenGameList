906f6554636d7c9a85ed4f9c9a2c37179c1e7d5f
serglotti <sergio.lottimore@gmail.com>
2016-11-04 23:14:54 -0300

    client side routing implemented (p146)

--- src/OpenGameListWebApp/Scripts/app/app.module.ts
+++ src/OpenGameListWebApp/Scripts/app/app.module.ts
@@ -3,23 +3,34 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 import "rxjs/Rx";
-import {AppComponent} from "./app.component";
-import {ItemListComponent} from "./item-list.component";
+import {AboutComponent} from "./about.component";import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home.component";
 import {ItemDetailComponent} from "./item-detail.component";
+import {ItemListComponent} from "./item-list.component";
+import {LoginComponent} from "./login.component";
+import {PageNotFoundComponent} from "./page-not-found.component";
+import {AppRouting} from "./app.routing";
 import {ItemService} from "./item.service";
 @NgModule({
  // directives, components, and pipes
  declarations: [
+    AboutComponent,
     AppComponent,
+    HomeComponent,
+    ItemDetailComponent,
     ItemListComponent,
-    ItemDetailComponent
+    LoginComponent,
+    PageNotFoundComponent
  ],
  // modules
  imports: [
      BrowserModule,
      HttpModule,
-     FormsModule
+     FormsModule,
+     RouterModule,
+     AppRouting
  ],
  // providers
  providers: [
